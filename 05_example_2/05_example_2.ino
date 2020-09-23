975e1c3025765ad65fc0fe2140bf71871c954d99
anjiwon319 <anjiwon319@kookmin.ac.kr>
2020-09-23 19:22:57 +0900

    05_example_2

---
+++ 05_example_2/05_example_2.ino
@@ -0,0 +1,27 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  while(!Serial){
+    ; //wait for serial port to connect
+  }
+  count = 0;
+}
+
+void loop() {
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+  while(count<5){
+    digitalWrite(PIN_LED, 1);
+    delay(100);
+    digitalWrite(PIN_LED, 0);
+    delay(100);
+    count +=1;
+   digitalWrite(PIN_LED, 1);
+  }
+  while(1){
+    ;
+  }
+}
